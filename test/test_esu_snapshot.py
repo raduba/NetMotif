f7ae238adfec0cced02a1a90b2bc2be561ef7510
uellenberg <JonahUellenberg@hotmail.com>
2026-03-16 00:43:55 -0700

    Add directed graph snapshot tests
    These weren't added before because digraphs were broken. Now that they
    work, we should test for them. This was spot checked for correctness
    against NemoMapPy.

--- test/test_esu_snapshot.py
+++ test/test_esu_snapshot.py
@@ -1,26 +1,47 @@
 from lib import compute_label_frequencies, DATA_DIR
+from src.graph_types import GraphType
 
 
-def eg_test_helper(size: int, snapshot):
-    frequencies = compute_label_frequencies(str(DATA_DIR / "exampleGraph.txt"), size=size)
+def eg_test_helper(size: int, snapshot, graph_type: GraphType):
+    frequencies = compute_label_frequencies(str(DATA_DIR / "exampleGraph.txt"), size, graph_type)
     assert frequencies == snapshot
 
 
-def test_eg_size1(snapshot):
-    eg_test_helper(1, snapshot)
+def test_eg_size1u(snapshot):
+    eg_test_helper(1, snapshot, GraphType.UNDIRECTED)
 
 
-def test_eg_size2(snapshot):
-    eg_test_helper(2, snapshot)
+def test_eg_size1d(snapshot):
+    eg_test_helper(1, snapshot, GraphType.DIRECTED)
 
 
-def test_eg_size3(snapshot):
-    eg_test_helper(3, snapshot)
+def test_eg_size2u(snapshot):
+    eg_test_helper(2, snapshot, GraphType.UNDIRECTED)
 
 
-def test_eg_size4(snapshot):
-    eg_test_helper(4, snapshot)
+def test_eg_size2d(snapshot):
+    eg_test_helper(2, snapshot, GraphType.DIRECTED)
 
 
-def test_eg_size5(snapshot):
-    eg_test_helper(5, snapshot)
+def test_eg_size3u(snapshot):
+    eg_test_helper(3, snapshot, GraphType.UNDIRECTED)
+
+
+def test_eg_size3d(snapshot):
+    eg_test_helper(3, snapshot, GraphType.DIRECTED)
+
+
+def test_eg_size4u(snapshot):
+    eg_test_helper(4, snapshot, GraphType.UNDIRECTED)
+
+
+def test_eg_size4d(snapshot):
+    eg_test_helper(4, snapshot, GraphType.DIRECTED)
+
+
+def test_eg_size5u(snapshot):
+    eg_test_helper(5, snapshot, GraphType.UNDIRECTED)
+
+
+def test_eg_size5d(snapshot):
+    eg_test_helper(5, snapshot, GraphType.DIRECTED)
--- test/test_random_snapshot.py
+++ test/test_random_snapshot.py
@@ -4,30 +4,53 @@ from src.random_graph import generate_random_graphs
 from lib import DATA_DIR, get_label_frequencies
 
 SEED = 12345678
-sample_graph = Graph(GraphType.UNDIRECTED, str(DATA_DIR / "exampleGraph.txt"))
+sample_graph_u = Graph(GraphType.UNDIRECTED, str(DATA_DIR / "exampleGraph.txt"))
+sample_graph_d = Graph(GraphType.DIRECTED, str(DATA_DIR / "exampleGraph.txt"))
 
 
-def random_test_helper(size: int, snapshot):
-    random_graph = generate_random_graphs(sample_graph, 1, size, seed=SEED)[0]
+def random_test_helper(size: int, snapshot, graph_type: GraphType):
+    random_graph = generate_random_graphs(
+        sample_graph_u if graph_type == GraphType.UNDIRECTED else sample_graph_d, 1, size, seed=SEED
+    )[0]
     frequencies = get_label_frequencies(random_graph)
     assert frequencies == snapshot
 
 
-def test_random_size1(snapshot):
-    random_test_helper(1, snapshot)
+def test_random_size1u(snapshot):
+    random_test_helper(1, snapshot, GraphType.UNDIRECTED)
 
 
-def test_random_size2(snapshot):
-    random_test_helper(2, snapshot)
+def test_random_size1d(snapshot):
+    random_test_helper(1, snapshot, GraphType.DIRECTED)
 
 
-def test_random_size3(snapshot):
-    random_test_helper(3, snapshot)
+def test_random_size2u(snapshot):
+    random_test_helper(2, snapshot, GraphType.UNDIRECTED)
 
 
-def test_random_size4(snapshot):
-    random_test_helper(4, snapshot)
+def test_random_size2d(snapshot):
+    random_test_helper(2, snapshot, GraphType.DIRECTED)
 
 
-def test_random_size5(snapshot):
-    random_test_helper(5, snapshot)
+def test_random_size3u(snapshot):
+    random_test_helper(3, snapshot, GraphType.UNDIRECTED)
+
+
+def test_random_size3d(snapshot):
+    random_test_helper(3, snapshot, GraphType.DIRECTED)
+
+
+def test_random_size4u(snapshot):
+    random_test_helper(4, snapshot, GraphType.UNDIRECTED)
+
+
+def test_random_size4d(snapshot):
+    random_test_helper(4, snapshot, GraphType.DIRECTED)
+
+
+def test_random_size5u(snapshot):
+    random_test_helper(5, snapshot, GraphType.UNDIRECTED)
+
+
+def test_random_size5d(snapshot):
+    random_test_helper(5, snapshot, GraphType.DIRECTED)
